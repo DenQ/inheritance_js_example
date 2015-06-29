20584e009920841f34739952286f8b975ad3bd40
DenQ <denquick@gmail.com>
2015-06-29 19:36:28 +0300

    Super, hard code, for run method

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var Child = function() {};
 
 var extended = function(Child, Base) {
     Child.prototype = Object.create(Base.prototype);
-    Child.parentClass = Base.prototype;
+    Child.prototype.parentClass = Base.prototype;
+}
+var __super = function() {
+    this.parentClass.run();
 }
 
 extended(Child, Root);
@@ -12,7 +15,8 @@ Root.prototype.run = function() {
     console.log('Root. Run...');
 };
 Child.prototype.run = function() {
-    Root.prototype.run(this, arguments);
+    //Root.prototype.run(this, arguments);
+    __super.call(this)
     console.log('Child. Run...');
 };
 
