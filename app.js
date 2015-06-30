11b70c798101b75eba9b677b29a6b561227ef092
DenQ <denquick@gmail.com>
2015-06-30 18:25:10 +0400

    After work

--- app.js
+++ app.js
@@ -5,11 +5,27 @@ var Child1 = function() {};
 var extended = function(Child, Base) {
     Child.prototype = Object.create(Base.prototype);
     Child.prototype.parentClass = Base.prototype;
-    Child.prototype.super = function () {
-        var __obj = Child.prototype.parentClass;
-        var __key = null;
-        for (__key in __obj) ;
-        Child.prototype.parentClass[__key]();
+
+    var __obj = Child.prototype;
+    console.log('*****');
+    for (var __key in __obj) {
+            //__obj[__key]['__name__'] = __key;
+            //console.log( __key, ' === ', __obj[__key] );
+            //console.log( __key );
+            //__obj[__key].__name__ = __key;
+            //__obj[__key].__name__ = 1111;
+            //Child.prototype[__key]['__name__'] = __key;
+    }
+
+
+    Child.prototype.super = function() {
+        //console.log(this);
+        //console.dir( arguments.toString() );
+        //var __obj = Child.prototype;
+        //console.log(__obj);
+        //var __key = null;
+        //for (__key in __obj) ;
+        //Child.prototype.parentClass[__key]();
     }
 };
 
@@ -27,9 +43,16 @@ Child.prototype.run = function() {
 
 extended(Child1, Child);
 Child1.prototype.run = function() {
+    console.log(this.__name__);
     this.super();
     console.log('Child1. Run...');
 };
+//Child1.prototype.run1 = function() {
+//    console.log(this.__name__);
+//    //this.super(this);
+//    //console.dir( this.__p );
+//    console.log('Child1. Run1...');
+//};
 
 var child = new Child1();
 child.run();
